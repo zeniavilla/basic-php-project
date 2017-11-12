f29823dd970895658baad30a496afbdcb3c4feca
Zenia <zeniavilla@hotmail.com>
2017-11-12 09:40:12 -0800

    Initialize staff index with header, navigation, and footer

---
+++ public/staff/index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" media="all" href="../stylesheets/staff.css" />
+  <title>GBI</title>
+</head>
+<body>
+  <header>
+    <h1>GBI Staff Area</h1>
+  </header>
+
+  <navigation>
+    <ul>
+      <li><a href="index.php">Menu</a></li>
+    </ul>
+  </navigation>
+
+  <div id="content">
+  </div>
+
+  <footer>
+    &copy; <?php echo date('Y'); ?> Globe Bank
+  </footer>
+</body>
+</html>
