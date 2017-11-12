a6f7764de3714c47e8572fe2ad954c47a1a9eac2
Zenia <zeniavilla@hotmail.com>
2017-11-12 09:58:30 -0800

    Create reusable staff_header file

---
+++ private/shared/staff_header.php
@@ -0,0 +1,20 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" media="all" href="../stylesheets/staff.css" />
+  <title>GBI</title>
+</head>
+<body>
+  <header>
+    <h1>GBI Staff Area</h1>
+  </header>
+
+  <navigation>
+    <ul>
+      <li><a href="index.php">Menu</a></li>
+    </ul>
+  </navigation>
