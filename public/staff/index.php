d0901d314104e49b6ad474d004de018b41d2a5e9
Zenia <zeniavilla@hotmail.com>
2017-11-12 09:59:11 -0800

    Refactor private/index to include php header and footer

--- public/staff/index.php
+++ public/staff/index.php
@@ -1,29 +1,8 @@
-<!doctype html>
+<?php require_once('../../private/initialize.php'); ?>
 
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" media="all" href="../stylesheets/staff.css" />
-  <title>GBI</title>
-</head>
-<body>
-  <header>
-    <h1>GBI Staff Area</h1>
-  </header>
+<?php include('../../private/shared/staff_header.php'); ?>
 
-  <navigation>
-    <ul>
-      <li><a href="index.php">Menu</a></li>
-    </ul>
-  </navigation>
+<div id="content">
+</div>
 
-  <div id="content">
-  </div>
-
-  <footer>
-    &copy; <?php echo date('Y'); ?> Globe Bank
-  </footer>
-</body>
-</html>
+<?php include('../../private/shared/staff_footer.php'); ?>
