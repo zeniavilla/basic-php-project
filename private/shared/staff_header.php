06a70422afb5e8c475b35880704bae3425e819fb
Zenia <zeniavilla@hotmail.com>
2017-11-14 12:41:47 -0800

    Use url_for function in staff_header links

--- private/shared/staff_header.php
+++ private/shared/staff_header.php
@@ -9,7 +9,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" media="all" href="../stylesheets/staff.css" />
+  <link rel="stylesheet" media="all" href="<?php echo url_for('/stylesheets/staff.css'); ?>" />
   <title>GBI - <?php echo $page_title; ?></title>
 </head>
 <body>
@@ -19,6 +19,6 @@
 
   <navigation>
     <ul>
-      <li><a href="index.php">Menu</a></li>
+      <li><a href="<?php echo url_for('/staff/index.php'); ?>">Menu</a></li>
     </ul>
   </navigation>
