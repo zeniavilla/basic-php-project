06012a5b9432f0dbabb48089f25b4a38e23004da
Zenia <zeniavilla@hotmail.com>
2017-11-20 09:57:43 -0800

    Create error functions for 404 and 500 header responses

--- private/functions.php
+++ private/functions.php
@@ -19,4 +19,15 @@ function raw_u($string="") {
 function h($string="") {
   return htmlspecialchars($string);
 }
+
+function error_404() {
+  header($_SERVER["SERVER_PROTOCOL" . " 404 Not Found"]);
+  exit();
+}
+
+function error_500() {
+  header($_SERVER["SERVER_PROTOCOL" . " 500 Internal Server Error"]);
+  exit();
+}
+
 ?>
