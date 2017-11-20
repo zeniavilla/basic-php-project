957ece1db59851fa38fbccec088cc92923720f2e
Zenia <zeniavilla@hotmail.com>
2017-11-20 09:33:29 -0800

    Add footer, header, and $page_titel to subjects/show.php

--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -4,10 +4,19 @@
 
 $id = $_GET['id'] ?? '1';
 
-echo h($id);
-
 ?>
 
-<a href="show.php?name=<?php echo u('John Doe'); ?>">Link</a><br />
-<a href="show.php?company=<?php echo u('Widgets&More'); ?>">Link</a><br />
-<a href="show.php?query=<?php echo u('!#*?'); ?>">Link</a><br />
+<?php $page_title = "Show Subject"; ?>
+<?php include(SHARED_PATH . '/staff_header.php'); ?>
+
+<div id="content">
+  <a href="<?php echo url_for('/staff/subjects/index.php'); ?>">&laquo; Back to List</a><br />
+
+  Page ID: <?php echo h($id); ?><br />
+
+  <a href="show.php?name=<?php echo u('John Doe'); ?>">Link</a><br />
+  <a href="show.php?company=<?php echo u('Widgets&More'); ?>">Link</a><br />
+  <a href="show.php?query=<?php echo u('!#*?'); ?>">Link</a><br />
+</div>
+
+<?php include(SHARED_PATH . '/staff_footer.php'); ?>
