683e591412ed3e8f2a2b9ec1557e7a72bfecd395
Zenia <zeniavilla@hotmail.com>
2017-11-20 09:33:01 -0800

    Escape $page_title

--- private/shared/staff_header.php
+++ private/shared/staff_header.php
@@ -10,7 +10,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" media="all" href="<?php echo url_for('/stylesheets/staff.css'); ?>" />
-  <title>GBI - <?php echo $page_title; ?></title>
+  <title>GBI - <?php echo h($page_title); ?></title>
 </head>
 <body>
   <header>
