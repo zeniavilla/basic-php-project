80ff4bcc60fcd267b56eed638c33b043d470c3b4
Zenia <zeniavilla@hotmail.com>
2017-11-27 12:13:35 -0800

    Add functions to check if request is a post or get method

--- private/functions.php
+++ private/functions.php
@@ -35,4 +35,12 @@ function redirect_to($location) {
   exit;
 }
 
+function is_post_request() {
+  return $_SERVER['REQUEST_METHOD'] == 'POST';
+}
+
+function is_get_request() {
+  return $_SERVER['REQUEST_METHOD'] == 'GET';
+}
+
 ?>
